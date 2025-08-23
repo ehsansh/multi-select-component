ec34e1d15d9e4b0699786d6bfe88eb36ae53e978
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2025-08-23 15:32:05 +0330

    Add 3 icons using svg for chevron up, chevron down and check mark

---
+++ src/components/Icons/ChevronDown.tsx
@@ -0,0 +1,15 @@
+export const ChevronDown = ({ size = 20, color = 'black' }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+        <polyline points="6 9 12 15 18 9" />
+    </svg>
+);
