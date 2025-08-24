3ad2895aa3ab56b83b1457e94a8084af961d275c
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2025-08-24 08:54:43 +0330

    Focus input automatically when isDropDownOpen is true

--- src/components/InputComponent/InputComponent.tsx
+++ src/components/InputComponent/InputComponent.tsx
@@ -1,4 +1,5 @@
 import type { Dispatch, SetStateAction } from 'react';
+import { useRef, useEffect } from 'react';
 import styles from './InputComponent.module.scss';
 import ChevronUp from '@/components/Icons/ChevronUp';
 
@@ -8,9 +9,18 @@ interface Props {
 }
 
 const InputComponent = ({ setIsDropDownOpen, isDropDownOpen }: Props) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+        if (isDropDownOpen) {
+            inputRef.current?.focus();
+        } else {
+            inputRef.current?.blur();
+        }
+    }, [isDropDownOpen]);
+
     return (
         <div onClick={() => setIsDropDownOpen((prev: boolean) => !prev)} className={styles.wrapper}>
-            <input autoComplete="off" name="search" type="text" />
+            <input ref={inputRef} autoComplete="off" name="search" type="text" />
             <div className={`${styles.icons} ${isDropDownOpen ? styles.rotate : ''}`}>
                 <ChevronUp size={20} color="#6B7280" />
             </div>
